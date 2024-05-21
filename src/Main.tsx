c3a2ca337b964c3cc74f301f6d2a518bceb88451
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-21 17:26:02 -0300

    Update Main.tsx

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-
-function Document() {
+import { Suspense, useEffect, useState } from 'react'
+import Nav from './Nav'
+import { Anchor, Article, Section } from './types'
+import Content from './Content'
+import useFetchMd from './useFetchMd'
+import useParseMd from './useParseMd'
+import Loading from './Loading'
+import InThisArticle from './InThisArticle'
+import useBuildAnchors from './useBuildAnchors'
+interface DocumentProps {
+  articles: Article[]
+}
+function Document(props: DocumentProps) {
+  const { articles } = props
+  const [section, setSection] = useState<Section>(articles[0].sections[0])
+  const [data, setData] = useState<string>('')
+  const [error, setError] = useState<string | null>(null)
+  const [anchors, setAnchors] = useState<Anchor[]>([])
+  useEffect(() => {
+    const promise = async () => {
+      try {
+        const md = await useFetchMd(section.url)
+        const html = await useParseMd(md)
+        setData(html)
+        setAnchors(useBuildAnchors(html))
+      } catch (e: any) {
+        setError(e.message)
+      }
+    }
+    promise()
+  }, [section])
+  if (error) return <div>{error}</div>
   return (
     <div>
-      <span>nav</span>
-      <span>content</span>
+      <Nav articles={articles} section={section} setSection={setSection} />
+      <Suspense fallback={<Loading />}>
+        <Content html={data} />
+        <InThisArticle items={anchors} />
+      </Suspense>
     </div>
   )
 }
