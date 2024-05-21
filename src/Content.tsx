b1389fe70cedfda871aed2cb61d68b3e644c919f
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-21 17:25:45 -0300

    Update Content.tsx

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,33 +1,9 @@
-import { Suspense, useEffect, useState } from 'react'
-import { Section } from './types'
-import useFetchMd from './useFetchMd'
-import useParseMd from './useParseMd'
-import Loading from './Loading'
-
 interface Props {
-  section: Section
+  html: string
 }
 function Content(props: Props) {
-  const [data, setData] = useState<string>('')
-  const [error, setError] = useState<string | null>(null)
-  const { section } = props
-  useEffect(() => {
-    const promise = async () => {
-      try {
-        const md = await useFetchMd(section.url)
-        const html = await useParseMd(md)
-        setData(html)
-      } catch (e: any) {
-        setError(e.message)
-      }
-    }
-    promise()
-  }, [section])
-  return (
-    <Suspense fallback={<Loading />}>
-      {error ? <div>{error}</div> : <div dangerouslySetInnerHTML={{ __html: data }} />}
-    </Suspense>
-  )
+  const { html } = props
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
 
 export default Content
