61446d14c0afa110fbae95e6a2db26a9e039fb0f
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-05-21 13:34:31 -0300

    use the mocks/test.md

--- src/mocks/articles.ts
+++ src/mocks/articles.ts
@@ -8,13 +8,13 @@ const articles: Article[] = [
         title: 'What is Beatriz Bot?',
         hashtag: 'what-is-beatriz-bot',
         space: 0,
-        url: 'https://github.com/FrovaHappy/beatriz-bot/blob/main/readme.md'
+        url: 'https://raw.githubusercontent.com/FrovaHappy/doc-react/main/src/mocks/test.md'
       },
       {
         title: 'How to use Beatriz Bot?',
         space: 1,
         hashtag: 'how-to-use-beatriz-bot',
-        url: 'https://github.com/FrovaHappy/beatriz-bot/blob/main/readme.md'
+        url: 'https://raw.githubusercontent.com/FrovaHappy/doc-react/main/src/mocks/test.md'
       }
     ]
   }
